92bf356c0e6bbedf2932e42c0000e2431ccd33b4
josko <joskovahanka@seznam.cz>
2023-11-12 21:39:52 +0100

    Nastaveni formulare, vytvoreni potvrzeni udaju

--- src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistraceController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistraceController.java
@@ -1,7 +1,17 @@
 package cz.czechitas.java2webapps.ukol5.controller;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.TemporalUnit;
 
 /**
  * Kontroler obsluhující registraci účastníků dětského tábora.
@@ -9,9 +19,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class RegistraceController {
 
-  @GetMapping("/")
-  public String formular() {
-    return "formular";
-  }
+    @GetMapping("/")
+    public ModelAndView formular() {
+        ModelAndView modelAndView = new ModelAndView("/formular");
+        modelAndView.addObject("form", new RegistraceForm());
+        return modelAndView;
+    }
+
+    @PostMapping("/formular")
+    public Object form(@Valid @ModelAttribute("form") RegistraceForm form, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "/formular";
+        }
+        Period period = form.getDatumNarozeni().until(LocalDate.now());
+        int vek = period.getYears();
+
+        if (vek < 9 || vek > 15) {
+            bindingResult.addError(new FieldError("form", "datumNarozeni", "Vek je mimo limit"));
+            return "/formular";
+        }
+
+        return new ModelAndView("/vyplnenyFormular").addObject("form", form);
+
+    }
 
 }
---
+++ src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistraceForm.java
@@ -0,0 +1,164 @@
+package cz.czechitas.java2webapps.ukol5.controller;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.Objects;
+
+public final class RegistraceForm {
+    @NotNull
+    @NotEmpty
+    private  String pohlavi;
+
+    private  Long vek;
+
+    @NotNull
+    @NotEmpty
+    private  String jmeno;
+
+    @NotNull
+    @NotEmpty
+    private  String prijmeni;
+
+    @NotNull
+    private  LocalDate datumNarozeni;
+
+    @NotNull
+    @NotEmpty
+    private  String turnus;
+    private  String email;
+    private  String telefon;
+
+    public RegistraceForm(String pohlavi,
+                          Long vek,
+                          String jmeno,
+                          String prijmeni,
+                          LocalDate datumNarozeni,
+                          String turnus,
+                          String email,
+                          String telefon
+    ) {
+        this.pohlavi = pohlavi;
+        this.vek = vek;
+        this.jmeno = jmeno;
+        this.prijmeni = prijmeni;
+        this.datumNarozeni = datumNarozeni;
+        this.turnus = turnus;
+        this.email = email;
+        this.telefon = telefon;
+    }
+
+    public RegistraceForm() {
+
+        pohlavi = null;
+        vek = null;
+        jmeno = null;
+        prijmeni = null;
+        datumNarozeni =  null;
+        turnus = null;
+        email = null;
+        telefon = null;
+    }
+
+    public void setPohlavi(String pohlavi) {
+        this.pohlavi = pohlavi;
+    }
+
+    public void setVek(Long vek) {
+        this.vek = vek;
+    }
+
+    public void setJmeno(String jmeno) {
+        this.jmeno = jmeno;
+    }
+
+    public void setPrijmeni(String prijmeni) {
+        this.prijmeni = prijmeni;
+    }
+
+    public void setDatumNarozeni(LocalDate datumNarozeni) {
+        this.datumNarozeni = datumNarozeni;
+    }
+
+    public void setTurnus(String turnus) {
+        this.turnus = turnus;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+
+    public String getPohlavi() {
+        return pohlavi;
+    }
+
+    public Long getVek() {
+        return vek;
+    }
+
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public String getPrijmeni() {
+        return prijmeni;
+    }
+
+    public LocalDate getDatumNarozeni() {
+        return datumNarozeni;
+    }
+
+    public String getTurnus() {
+        return turnus;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (RegistraceForm) obj;
+        return Objects.equals(this.pohlavi, that.pohlavi) &&
+                Objects.equals(this.vek, that.vek) &&
+                Objects.equals(this.jmeno, that.jmeno) &&
+                Objects.equals(this.prijmeni, that.prijmeni) &&
+                Objects.equals(this.datumNarozeni, that.datumNarozeni) &&
+                Objects.equals(this.turnus, that.turnus) &&
+                Objects.equals(this.email, that.email) &&
+                Objects.equals(this.telefon, that.telefon);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pohlavi, vek, jmeno, prijmeni, datumNarozeni, turnus, email, telefon);
+    }
+
+    @Override
+    public String toString() {
+        return "RegistraceForm[" +
+                "pohlavi=" + pohlavi + ", " +
+                "vek=" + vek + ", " +
+                "jmeno=" + jmeno + ", " +
+                "prijmeni=" + prijmeni + ", " +
+                "datumNarozeni=" + datumNarozeni + ", " +
+                "turnus=" + turnus + ", " +
+                "email=" + email + ", " +
+                "telefon=" + telefon + ']';
+    }
+
+}
